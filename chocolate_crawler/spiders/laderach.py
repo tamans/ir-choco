41a4c47b6bb57492ed8f6008cda90db8f7d1c973
tamans <tsapana99@gmail.com>
2023-11-23 22:37:43 +0100

    fixed laderach_spider

--- chocolate_crawler/spiders/laderach.py
+++ chocolate_crawler/spiders/laderach.py
@@ -1,24 +1,69 @@
+import logging
+from typing import Any, Optional
 import scrapy
+from scrapy.selector import Selector
 
-class ChocolateSpider(scrapy.Spider):
-    name = 'chocolate_spider'
+class LaderachSpider(scrapy.Spider):
+    name = 'laderach_spider'
     allowed_domains = ['laderach.com']
-    start_urls = ['https://laderach.com']
+    
+    def __init__(self, tags=None, *args, **kwargs):
+
+        self.start_urls = []
+
+        if tags == 'null':
+            self.tags = []
+        elif tags:
+            self.tags = tags.split(',')
+        else:
+            self.tags = ['alle-produkte', 'geschenke', 'frischschoggitm', 'pralines-truffes',
+                        'tafeln','snacking','vegan', 'celebration-gifts', 'gifts-for-sharing',
+                        'thank-you-gifts', 'greeting-cards']
+
+        self.start_urls = [f'https://laderach.com/ch-en/{tag}' for tag in self.tags]
+
+        super().__init__(**kwargs)
+        
 
     def parse(self, response):
         # Extract information about chocolate from the webpage
-        chocolate_info = {
-            'docno': response.url,  # Replace with a suitable identifier
-            'title': response.css('h1::text').get(),
-            'description': response.css('p::text').get(),
-            'ingredients': response.css('div.ingredients::text').get(),
-            'price': response.css('span.price::text').get(),
+        laderach_info = {
+            'site': self.name,
+            'page_link': response.url,  # Replace with a suitable identifier
+            'title': response.css('h1::text').get() or response.css('h2::text').get() or response.css('h3::text').get(),
+            'description': response.css('div.product-facts::text').get() 
+                            or response.css('div.main-text::text').get() or response.css('div.category-view.container > p::text').get(),
+            'ingredients': self.extract_ingredients(response),
+            'allergens': self.extract_additional_info(response),
+            'price': response.css('span.price::text').get() or response.css('div.price-box.price-final_price').get(),
         }
 
         # Check if any relevant data is present
-        if any(chocolate_info.values()):
-            yield chocolate_info
+        if all( value for value in laderach_info.values()):
+            yield laderach_info
+        # else:
+        #     logging.warning(f"Missing data for {response.url}. Skipping.")
 
         # Follow links to other pages if needed
         for next_page in response.css('a::attr(href)'):
             yield response.follow(next_page, self.parse)
+
+    # combines the underlined and normal ingredient elements
+    def extract_ingredients(self, response):
+        # Extracting text content of nodes following the "Ingredients" label
+        ingredients_nodes = response.xpath('//strong[contains(text(), "Ingredients")]/following-sibling::text()').extract()
+        ingredients_text = ' '.join(ingredients_nodes).strip()
+
+        # Extracting text content of <u> elements within <p> tags
+        additional_ingredients = response.xpath('//p//u/text()').extract()
+        additional_ingredients_text = ' '.join(additional_ingredients).strip()
+
+        # Combine both sets of ingredients
+        combined_ingredients = f'{ingredients_text} {additional_ingredients_text}'
+
+        return combined_ingredients.strip()
+
+    # Allergens 
+    def extract_additional_info(self, response):
+        additional_info = response.css('span.alt-edited::text').get()
+        return additional_info.strip() if additional_info else None
--- chocolate_crawler/spiders/lindt.py
+++ chocolate_crawler/spiders/lindt.py
@@ -1,24 +1,61 @@
+from typing import Any, Optional
 import scrapy
 
 class LindtChocolatierSpider(scrapy.Spider):
-    name = 'lindtchocolatier_spider'
-    # allowed_domains = ['en.maxchocolatier.com']
-    start_urls = ['https://www.lindt.ch/de/']
+    name = 'lindt_spider'
+    allowed_domains = ['https://www.chocolate.lindt.com']
+
+    def __init__(self, tags=None, *args, **kwargs):
+
+        self.start_urls = []
+
+        if tags == 'null':
+            self.tags = []
+        elif tags:
+            self.tags = tags.split(',')
+        else:
+            self.tags = ['our-chocolate'
+                        #  'our-chocolate/our-brands', 'our-chocolate/our-brands/lindor', 'our-brands/excellence',
+                        # 'our-chocolate/our-brands/pralines','our-chocolate/our-brands/creation','our-chocolate/our-brands/hello', 'our-chocolate/gift-ideas', 
+                        # 'our-chocolate/type',
+                        # 'our-chocolate/type/dark-chocolate', 'our-chocolate/type/white-chocolate', 'our-chocolate/type/milk-chocolate'
+                        ]
+
+        self.start_urls = [f'https://www.chocolate.lindt.com/{tag}' for tag in self.tags]
+
+        super().__init__(**kwargs)
 
     def parse(self, response):
+
+        lindt_info = {
+            'site': self.name,
+            'page_link': response.url,  # Replace with a suitable identifier
+            'title': response.css('h1::text').get() or response.css('h2::text').get() or response.css('h3::text').get(),
+            'description': response.css('div.product.attribute.description::text').get(),
+            'ingredients': response.css('span.ingredient-label:contains("Ingredients:") + p::text').get(),
+            'allergens': response.css('span.ingredient-label.contains("Allergens:") + p::text').get(),
+            'cacao solids': response.css('span.ingredient-label.contains("Cacao Solids %") + p::text').get(),
+            'price': response.css('span.price::text').get() or response.css('div.price-box.price-final_price').get(),
+        }
+
+        # Check if any relevant data is present
+        if all( value for value in lindt_info.values()):
+            yield lindt_info
+        # else:
+        #     logging.warning(f"Missing data for {response.url}. Skipping.")
        
-        products = response.xpath('//*[@id="w-node-_5bb703ca-8687-69b9-7189-c26191c5fd53-f5d46110"]')  # Replace with the actual XPath for product items
+        # products = response.xpath('//*[@id="w-node-_5bb703ca-8687-69b9-7189-c26191c5fd53-f5d46110"]')  # Replace with the actual XPath for product items
 
-        for product in products:
-            product_info = {
-                'name': product.xpath('a[1]/text()').get(),
-                'description': product.xpath('p/text()').get(),
-                'price': product.xpath('div[2]/p[1]/text()').get(),
-            }
+        # for product in products:
+        #     product_info = {
+        #         'name': product.xpath('a[1]/text()').get(),
+        #         'description': product.xpath('p/text()').get(),
+        #         'price': product.xpath('div[2]/p[1]/text()').get(),
+        #     }
 
-            # Check if any relevant data is present
-            if any(product_info.values()):
-                yield product_info
+        #     # Check if any relevant data is present
+        #     if any(product_info.values()):
+        #         yield product_info
 
         # Follow links to other pages if needed
         for next_page in response.css('a.next::attr(href)'):
