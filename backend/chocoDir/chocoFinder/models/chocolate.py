dd5996afe17a3a288a2ea46cd48aa27cac114815
tamans <tsapana99@gmail.com>
2023-12-02 17:16:14 +0100

    update backend

--- backend/chocoDir/chocoFinder/models/chocolate.py
+++ backend/chocoDir/chocoFinder/models/chocolate.py
@@ -5,11 +5,13 @@ from django.db import models;
 
 class Chocolate(models.Model):
     docno = models.CharField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
+    site = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
     ingredients = models.JSONField(default=list)
+    allergens = models.JSONField(default=list)
     price = models.CharField(max_length=20)
 
 
 def __str__(self):
-    return self.name
+    return self.title
---
+++ backend/chocoDir/index_parser.py
@@ -0,0 +1,40 @@
+import os
+import django
+import csv
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'choco.settings')  
+
+django.setup()
+
+from chocoFinder.models.chocolate import Chocolate
+
+csv_file_path = os.path.abspath('./../../Indexing/index/chocolate.csv') 
+chocolates = []
+
+Chocolate.objects.all().delete()  
+
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    for row in csv_reader:
+        chocolate = Chocolate(
+            docno=row['docno'],
+            title=row['title'],
+            description=row['description'],
+            ingredients=row['ingredients'],
+            allergens=row['allergens'],
+            price=row['price']
+        )
+        chocolates.append(chocolate)
+
+# Bulk insert the records into the database
+Chocolate.objects.bulk_create(chocolates)
+
+print('Chocolate added successfully')
+
+# Retrieve the inserted objects
+inserted_chocolates = Chocolate.objects.filter(docno__in=[choco.docno for choco in chocolates])
+
+# Now, inserted_chocolates contains the Chocolate objects that were just inserted
+for choco in inserted_chocolates:
+    print(f"Inserted Chocolate: {choco.docno}, {choco.title}")
